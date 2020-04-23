3e6f5ca0a87ad34518bdac1f74a9181f2b482f79
Kyle McLean <kyledmclean@gmail.com>
2020-04-23 18:11:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,3 +5,6 @@ katz_deli = []
 def take_a_number(katz_deli, name)
   katz_deli.shift = name
   puts "Welcome, #{name}.  You are number #{katz_deli.size} in line."
+end
+
+take_a_number(katz_deli, "Peter")
